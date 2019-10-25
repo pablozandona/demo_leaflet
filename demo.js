3f18711f990ed26772d7ecc762dbb59dfbcd42fe
Pablo Zandoná <P!a@4679>
2019-10-25 18:28:56 -0300

    geojson

--- demo.js
+++ demo.js
@@ -1,14 +1,40 @@
-function init() {
+let geoJson;
+fetch( './municipios_sc.json').then(r => r.json()).then(j => geoJson = j);
 
-    var lat = -27.596910;
-    var lng = -48.549580;
-    var zoomLevel = 13;
+async function init() {
 
-    var map = L.map('map').setView([lat, lng], zoomLevel);
+    const latLangUnivali = [-27.598023, -48.619444];
 
-    // Cria a layer para inserir no mapa
-    var mainLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        // Créditos da layer 
+    const map = L.map('map').setView(latLangUnivali, 16);
+
+    // Layer do OpenStreetMaps
+    const openstreetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    openstreetmap.addTo(map);
+
+    // Popup
+    const univaliPopup = '<img width="100" src="https://www.univali.br/Style%20Library/Univali/custom/marca/imgs/logo.png" />';
+
+    // Maker
+    const univaliMarker = L.marker(latLangUnivali)
+        .bindPopup(univaliPopup);
+
+    univaliMarker.addTo(map);
+
+    // Layer do MapBox
+    const stamen = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 1,
+        maxZoom: 16,
+        ext: 'jpg'
+    });
+
+    // Layer do MapBox
+    const mapbox = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        // Créditos da layer
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         minZoom: 1,
@@ -17,52 +43,40 @@ function init() {
         accessToken: 'pk.eyJ1IjoibHVpemhzYWxhYXIiLCJhIjoiY2syNjhuNXE2MXBubDNjbjB1MDdpa2p1aCJ9.Xo1g0NzO6lxnkww5lo14-Q'
     });
 
-    // Layer sem token de acesso
-    var openstreet_map = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-
     // Layer de topografia
-    var openstreet_map_topografia = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    const openstreetmap_topografia = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         maxZoom: 17,
         attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     });
 
     // Layer de transporte
-    var openstreet_map_transporte = L.tileLayer('http://openptmap.org/tiles/{z}/{x}/{y}.png', {
+    const openstreetmap_transporte = L.tileLayer('http://openptmap.org/tiles/{z}/{x}/{y}.png', {
         maxZoom: 17,
         attribution: 'Map data: &copy; <a href="http://www.openptmap.org">OpenPtMap</a> contributors'
+    });
+
+    // Layer com GeoJson - Municípios de SC
+    const geojson_layer = L.geoJSON(geoJson , {
+        "color": "#ff7800",
+        "weight": 5,
+        "opacity": 0.65
     });
 
     // Extensão do leaflet com várias layers gratuitas
     // github: https://github.com/leaflet-extras/leaflet-providers
     // demo: https://leaflet-extras.github.io/leaflet-providers/preview/
- 
-    // Adiciona a layer no mapa
-    mainLayer.addTo(map);
-
-    var latUnivali = -27.598023;
-    var lngUnivali = -48.619444;
-
-    var univaliMarker = L.marker(
-        [latUnivali, lngUnivali],
-        {
-            title: 'Campus Univali Kobrasol'
-        }
-    ).bindPopup('Hello, friends!');
-
-    univaliMarker.addTo(map);
 
     L.control.layers(
         {
-            "Principal": mainLayer,            
-            "Topografia": openstreet_map_topografia,
-            "Street Map": openstreet_map
+            "OpenStreetMaps": openstreetmap,
+            "Topografia": openstreetmap_topografia,
+            "MapBox": mapbox,
+            "Stamen Watercolor": stamen,
         },
         // Overlay na layer principal
         {
-            "Transporte": openstreet_map_transporte
+            "Transporte": openstreetmap_transporte,
+            "GeoJson": geojson_layer
         }
     ).addTo(map);
 
